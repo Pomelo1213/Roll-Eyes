9c8d654c339d548d739a3fd63137ba22ea937648
luoxin <goodmanluoxin@163.com>
2018-02-10 12:48:05 +0800

    Roll-Eyes

---
+++ main.js
@@ -0,0 +1,19 @@
+document.addEventListener('mousemove', function(e){
+    var mouseX = e.clientX
+    var mouseY = e.clientY
+
+    var leftE = {x: 700, y: 200}
+    var rightE = {x: 1200, y: 200}
+
+    var leftRotateY = (mouseX - leftE.x) / 70
+    var leftRotateX = (mouseY - leftE.y) / 70
+    //console.log(mouseY+" "+leftE.y)
+    console.log(leftRotateX)
+
+    leftRotateX = -leftRotateX
+    var leftEye = document.getElementById("one")
+    leftEye.style.cssText = `transform: rotateX(${leftRotateX}deg) rotateY(${leftRotateY}deg)`
+
+    var rightEye = document.getElementById("two")
+    rightEye.style.cssText = `transform: rotateX(${leftRotateX}deg) rotateY(${leftRotateY}deg)`
+})
